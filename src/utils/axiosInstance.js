a316e1d7f12f3a37f8c0a28b75b970830b8cfddc
rastogi20 <rastogisahil20@gmail.com>
2024-11-21 12:34:42 +0530

    connected to the server

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://travel-story-backend-us3b.onrender.com/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
